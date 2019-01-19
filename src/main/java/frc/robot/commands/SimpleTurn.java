779522d45bc84de346ffdf9d06d067829bb0ba46
briforge <briforge@gmail.com>
2019-01-19 12:07:34 -0500

    Green LED grip changes

---
+++ src/main/java/frc/robot/commands/SimpleTurn.java
@@ -0,0 +1,49 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+public class SimpleTurn extends Command {
+  public SimpleTurn() {
+    requires(Robot.drivetrain);
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    setTimeout(2);  // set 2 second timeout
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+      Robot.drivetrain.turn(0.50);
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return isTimedOut();
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+    Robot.drivetrain.stop();
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
---
+++ src/main/java/frc/robot/commands/TurnRelative.java
@@ -0,0 +1,83 @@
+package frc.robot.commands;
+
+import frc.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class TurnRelative extends Command {
+	double targetAngle;
+	double degreeTolerance;
+	int onTargetCount = 0;
+    public TurnRelative(double targetAngle) {
+    	requires(Robot.drivetrain);
+    	this.targetAngle = targetAngle;
+    	degreeTolerance = 3.0;
+    }
+    
+    public TurnRelative(double targetAngle, double degreeTolerance) {
+    	requires(Robot.drivetrain);
+    	this.targetAngle = targetAngle;
+    	this.degreeTolerance = degreeTolerance;
+    }
+
+    protected void initialize() {
+    	Robot.drivetrain.resetGyro();
+    	double currentAngle = Robot.drivetrain.getHeading();
+    	//Robot.drivetrain.turnPID.setPercentTolerance(3.0/360.0);
+    	Robot.drivetrain.turnPID.setSetpoint(targetAngle);
+    	Robot.drivetrain.turnPID.enable();
+    	Robot.drivetrain.shiftLow();
+    }
+
+    protected void execute() {
+    	Robot.drivetrain.pidTurn();
+    	SmartDashboard.putNumber("onTargetCount", onTargetCount);
+    }
+    
+    private void log(Object msg) {
+    	System.out.println(msg);
+    }
+    
+    private boolean isOnTarget() {
+    	//return Math.abs(Robot.drivetrain.getHeading() - targetAngle) < degreeTolerance;  //if the robot is within 1 degrees of the target, stop
+    	
+    	if (Math.abs(Robot.drivetrain.getHeading() - targetAngle) < 1.0) {
+    		//if (Robot.drivetrain.turnPID.get() < 0.1 && Robot.drivetrain.turnPID.get() > -0.1) {
+    			//return true;
+    		//}
+    		return true;
+    	}
+    	
+    	
+    	return false;
+    }
+        
+    protected boolean isFinished() {
+    	SmartDashboard.putNumber("OnTargetCount", onTargetCount);
+    	if (isOnTarget()) {
+    		onTargetCount++;
+    	} else {
+    		onTargetCount = 0;
+    	}
+    	
+    	if(onTargetCount > 1) {
+    		log("finished turn at " + Robot.drivetrain.getHeading() + "angle");
+    		return true;
+    	} else {
+    		return false;
+    	}
+    }
+
+    protected void end() {
+    	Robot.drivetrain.stop();
+    	Robot.drivetrain.turnPID.disable();
+    	onTargetCount = 0;
+    }
+
+    protected void interrupted() {
+    	Robot.drivetrain.stop();
+    	Robot.drivetrain.turnPID.disable();
+    	onTargetCount = 0;
+    }
+}
